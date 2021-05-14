42ce80a7fa04238009be4edd7189ad3c4bbc402c
Snivo <jrgerba@gmail.com>
2021-05-14 01:58:49 -0400

    Removed yield return

--- PagedArray.cs
+++ PagedArray.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ECS
 {
-    class PagedArray<T> : IEnumerable<T>
+    class PagedArray<T>
     {
         T[][] elements;
        
@@ -50,30 +50,6 @@ namespace ECS
 
             elements[page] = new T[PageSize];
         }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            int idx = 0;
-            for (int page = 0; page < elements.Length; page++)
-            {
-                T[] curPage = elements[page];
-
-                if (curPage == null)
-                        continue;
-
-                for (int i = 0; i < curPage.Length; i++)
-                {
-                    T obj = curPage[i];
-                    if (obj == null)
-                        continue;
-
-                    yield return obj;
-                }
-                idx += PageSize;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public PagedArray(int pageSize = 1024)
         {
